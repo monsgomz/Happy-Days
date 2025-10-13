fc025b8454ce14befa53682ee05cb3e4ec229939
Montserrat Gomez <montserrat.ga95@gmail.com>
2025-10-13 13:55:18 -0600

    Se añaden outlets

---
+++ HappyDays/HappyDays/MemorieCellCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MemorieCellCollectionViewCell.swift
+//  HappyDays
+//
+//  Created by Montserrat Gomez on 13/10/25.
+//
+
+import UIKit
+
+class MemorieCellCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ HappyDays/HappyDays/MemoriesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MemoriesViewController.swift
+//  HappyDays
+//
+//  Created by Montserrat Gomez on 13/10/25.
+//
+
+import UIKit
+
+class MemoriesViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+	@IBOutlet var imageView: UIImageView!
+	
+
+
+}
